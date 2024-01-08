0be2d94058bcb9808f859d2fdfb2548f6add79ee
Tom Depussay <todepussay@stpbb.org>
2024-01-08 09:33:53 +0100

    Correction db

--- db_template.sql
+++ db_template.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE IF NOT EXISTS `chat` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `chat`;
 
-CREATE TABLE IF NOT EXISTS "t_user" (
-    "id" INT NOT NULL AUTO_INCREMENT,
-    "name" varchar(255) NOT NULL,
-    "password" varchar(255) NOT NULL,
-    "email" varchar(255) NOT NULL,
-    "create_time" datetime NOT NULL,
-    "update_time" datetime NOT NULL,
-    PRIMARY KEY ("id")
+CREATE TABLE IF NOT EXISTS `t_user`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `create_time` datetime NOT NULL,
+    `update_time` datetime NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `t_conversation` (
