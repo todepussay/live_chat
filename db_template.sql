eb9c9a1a5c0a435707bde3fdad317b5e0a660d52
Tom Depussay <todepussay@stpbb.org>
2024-01-15 21:23:46 +0100

    update db

--- db_template.sql
+++ db_template.sql
@@ -12,30 +12,60 @@ CREATE TABLE IF NOT EXISTS `t_user`(
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `t_conversation` (
+CREATE TABLE IF NOT EXISTS `l_relation`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `id_user1` INT NOT NULL,
     `id_user2` INT NOT NULL,
+    `status` varchar(255) NOT NULL,
     `create_time` datetime NOT NULL,
     `update_time` datetime NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `t_conversation` ADD CONSTRAINT `fk_t_conversation_t_user1` FOREIGN KEY (`id_user1`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE `t_conversation` ADD CONSTRAINT `fk_t_conversation_t_user2` FOREIGN KEY (`id_user2`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `l_relation` ADD CONSTRAINT `fk_l_relation_t_user1` FOREIGN KEY (`id_user1`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `l_relation` ADD CONSTRAINT `fk_l_relation_t_user2` FOREIGN KEY (`id_user2`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-CREATE TABLE IF NOT EXISTS `t_message` (
+CREATE TABLE IF NOT EXISTS `t_group`(
     `id` INT NOT NULL AUTO_INCREMENT,
-    `id_conversation` INT NOT NULL,
-    `id_user` INT NOT NULL,
-    `content` TEXT NOT NULL,
-    `send_time` datetime NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `create_time` datetime NOT NULL,
+    `update_time` datetime NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `t_message` ADD CONSTRAINT `fk_t_message_t_conversation` FOREIGN KEY (`id_conversation`) REFERENCES `t_conversation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE `t_message` ADD CONSTRAINT `fk_t_message_t_user` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+CREATE TABLE IF NOT EXISTS `l_group_user`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `id_group` INT NOT NULL,
+    `id_user` INT NOT NULL,
+    `create_time` datetime NOT NULL,
+    `update_time` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+ALTER TABLE `l_group_user` ADD CONSTRAINT `fk_l_group_user_t_group` FOREIGN KEY (`id_group`) REFERENCES `t_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `l_group_user` ADD CONSTRAINT `fk_l_group_user_t_user` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE TABLE IF NOT EXISTS `t_message`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `id_user` INT NOT NULL,
+    `id_group` INT NOT NULL,
+    `system` boolean NOT NULL DEFAULT false,
+    `content` TEXT NOT NULL,
+    `create_time` datetime NOT NULL,
+    `update_time` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `t_message` ADD CONSTRAINT `fk_t_message_t_user` FOREIGN KEY (`id_user`) REFERENCES `t_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `t_message` ADD CONSTRAINT `fk_t_message_t_group` FOREIGN KEY (`id_group`) REFERENCES `t_group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE TABLE IF NOT EXISTS `l_user_message`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `id_user` INT NOT NULL,
+    `id_message` INT NOT NULL,
+    `time` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DELIMITER //
 CREATE TRIGGER after_delete_conversation
@@ -59,3 +89,5 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+
